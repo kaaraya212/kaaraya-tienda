f1c1635b47bfc18b83a6074258d14d7303797f90
kaaraya212 <148176715+kaaraya212@users.noreply.github.com>
2025-09-22 19:53:25 -0600

    New
    Adding new config

---
+++ src/test/java/TiendaTech/com/demo/TiendaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaTech.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
